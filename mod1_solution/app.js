17c28a8e6e4b3a00b0ceb66406a2685d4ab00350
Your Name <you@example.com>
2016-10-07 16:30:54 -0500

    Lunch Check

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,6 +1,28 @@
 (function () {
 'use strict';
-angular.module('LunchCheck',[])
-.controller('LunchCheckController', LunchCheckController);
 
-}
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', MyFirstController) ;
+  MyFirstController.$inject = ['$scope'];
+  function MyFirstController ($scope) {
+    $scope.stringtosplit="";
+    $scope.howMuch="";
+
+  $scope.CalculateHowMuch = function(){
+    var instring=$scope.stringtosplit;
+    // var Arrayofwords = string.split(',');
+    var Arrayofwords=instring.split(',');
+    if (Arrayofwords[0] == "") {
+      $scope.howMuch="Please enter data first"
+    } else if (Arrayofwords.length > 3 ) {
+      $scope.howMuch="Too Much!"
+    } else {
+      $scope.howMuch="Enjoy!"
+    }
+
+  };
+
+};
+
+})();
